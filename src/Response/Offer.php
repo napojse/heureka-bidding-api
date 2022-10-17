651b4fa51ee31709cb8eee1610259ae4fc36c934
Mirek Osif <mirek@emayl.ru>
2022-10-17 14:24:00 +0200

    Add shop offer attribute

---
+++ src/Response/Offer.php
@@ -0,0 +1,151 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Baraja\HeurekaBiddingApi\Response;
+
+
+final class Offer extends BaseResponse
+{
+
+    private int $id;
+
+    private string $itemId;
+
+    private string $name;
+
+    private ?int $averageDeliveryDays;
+
+    private float $price;
+
+    private float $minDeliveryPrice;
+
+    private bool $shoppingCart;
+
+    /** @var mixed[] */
+    private array $attributes;
+
+    /** @var mixed[] */
+    private array $gifts;
+
+    /** @var mixed[] */
+    private array $availability;
+
+    /**
+     * @param mixed[] $haystack
+     */
+    public function __construct(array $haystack)
+    {
+        $this->id = $haystack['id'];
+        $this->itemId = $haystack['item_id'];
+        $this->name = $haystack['name'];
+        $this->averageDeliveryDays = $haystack['average_delivery_days'];
+        $this->price = (float) $haystack['price'];
+        $this->minDeliveryPrice = (float) $haystack['min_delivery_price'];
+        $this->shoppingCart = $haystack['shopping_cart'];
+        $this->attributes = $haystack['attributes'];
+        $this->gifts = $haystack['gifts'];
+        $this->availability = $haystack['availability'];
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getItemId(): string
+    {
+        return $this->itemId;
+    }
+
+    public function setItemId(string $itemId): void
+    {
+        $this->itemId = $itemId;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getAverageDeliveryDays(): ?int
+    {
+        return $this->averageDeliveryDays;
+    }
+
+    public function setAverageDeliveryDays(int $averageDeliveryDays): void
+    {
+        $this->averageDeliveryDays = $averageDeliveryDays;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function getMinDeliveryPrice(): float
+    {
+        return $this->minDeliveryPrice;
+    }
+
+    public function setMinDeliveryPrice(float $minDeliveryPrice): void
+    {
+        $this->minDeliveryPrice = $minDeliveryPrice;
+    }
+
+    public function getShoppingCart(): bool
+    {
+        return $this->shoppingCart;
+    }
+
+    public function setShoppingCart(bool $shoppingCart): void
+    {
+        $this->shoppingCart = $shoppingCart;
+    }
+
+    public function getAttributes(): array
+    {
+        return $this->attributes;
+    }
+
+    public function setAttributes(array $attributes): void
+    {
+        $this->attributes = $attributes;
+    }
+
+    public function getGifts(): array
+    {
+        return $this->gifts;
+    }
+
+    public function setGifts(array $gifts): void
+    {
+        $this->gifts = $gifts;
+    }
+
+    public function getAvailability(): array
+    {
+        return $this->availability;
+    }
+
+    public function setAvailability(array $availability): void
+    {
+        $this->availability = $availability;
+    }
+
+}
--- src/Response/Shop.php
+++ src/Response/Shop.php
@@ -7,118 +7,139 @@ namespace Baraja\HeurekaBiddingApi\Response;
 
 final class Shop extends BaseResponse
 {
-	private int $id;
+    private int $id;
 
-	private string $slug;
+    private string $slug;
 
-	private string $name;
+    private string $name;
 
-	private string $homepage;
+    private string $homepage;
 
-	/** @var mixed[]|null */
-	private ?array $verifiedByCustomersStatus;
+    /** @var mixed[]|null */
+    private ?array $verifiedByCustomersStatus;
 
-	private bool $certifiedSeller;
+    private bool $certifiedSeller;
 
-	/** @var mixed|null */
-	private $shopOfTheYear;
+    /** @var mixed|null */
+    private $shopOfTheYear;
 
-	private ?int $rating;
+    private ?int $rating;
 
-	private ?int $ratingCount;
+    private ?int $ratingCount;
 
-	private ?int $reviewCount;
+    private ?int $reviewCount;
 
-	private bool $cashBackQuarantee;
+    private bool $cashBackQuarantee;
 
+    /** @var Offer[] */
+    private array $offers = [];
 
-	/**
-	 * @param mixed[] $haystack
-	 */
-	public function __construct(array $haystack)
-	{
-		$this->id = $haystack['id'];
-		$this->slug = $haystack['slug'];
-		$this->name = $haystack['name'];
-		$this->homepage = $haystack['homepage'];
-		$this->verifiedByCustomersStatus = $haystack['verified_by_customers_status'];
-		$this->certifiedSeller = $haystack['is_certified_seller'];
-		$this->shopOfTheYear = $haystack['shop_of_the_year'];
-		$this->rating = $haystack['rating'];
-		$this->ratingCount = $haystack['rating_count'];
-		$this->reviewCount = $haystack['review_count'];
-		$this->cashBackQuarantee = $haystack['has_cashback_guarantee'];
-	}
+    /**
+     * @param mixed[] $haystack
+     */
+    public function __construct(array $haystack)
+    {
+        $this->id = $haystack['id'];
+        $this->slug = $haystack['slug'];
+        $this->name = $haystack['name'];
+        $this->homepage = $haystack['homepage'];
+        $this->verifiedByCustomersStatus = $haystack['verified_by_customers_status'];
+        $this->certifiedSeller = $haystack['is_certified_seller'];
+        $this->shopOfTheYear = $haystack['shop_of_the_year'];
+        $this->rating = $haystack['rating'];
+        $this->ratingCount = $haystack['rating_count'];
+        $this->reviewCount = $haystack['review_count'];
+        $this->cashBackQuarantee = $haystack['has_cashback_guarantee'];
 
+        if (isset($haystack['offer']) === true) {
+            $this->addOffer(new Offer($haystack['offer']));
+        } elseif (isset($haystack['offers']) === true) {
+            foreach ($haystack['offers'] as $offer) {
+                $this->addOffer(new Offer($offer));
+            }
+        }
+    }
 
-	public function getId(): int
-	{
-		return $this->id;
-	}
+    public function getId(): int
+    {
+        return $this->id;
+    }
 
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
 
-	public function getSlug(): string
-	{
-		return $this->slug;
-	}
+    public function getName(): string
+    {
+        return $this->name;
+    }
 
+    public function getHomepage(): string
+    {
+        return $this->homepage;
+    }
 
-	public function getName(): string
-	{
-		return $this->name;
-	}
+    /**
+     * @return mixed[]|null
+     */
+    public function getVerifiedByCustomersStatus(): ?array
+    {
+        return $this->verifiedByCustomersStatus;
+    }
 
+    public function isCertifiedSeller(): bool
+    {
+        return $this->certifiedSeller;
+    }
 
-	public function getHomepage(): string
-	{
-		return $this->homepage;
-	}
+    /**
+     * @return mixed|null
+     */
+    public function getShopOfTheYear()
+    {
+        return $this->shopOfTheYear;
+    }
 
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
 
-	/**
-	 * @return mixed[]|null
-	 */
-	public function getVerifiedByCustomersStatus(): ?array
-	{
-		return $this->verifiedByCustomersStatus;
-	}
+    public function getRatingCount(): ?int
+    {
+        return $this->ratingCount;
+    }
 
+    public function getReviewCount(): ?int
+    {
+        return $this->reviewCount;
+    }
 
-	public function isCertifiedSeller(): bool
-	{
-		return $this->certifiedSeller;
-	}
+    public function isCashBackQuarantee(): bool
+    {
+        return $this->cashBackQuarantee;
+    }
 
+    /**
+     * @return Offer[]
+     */
+    public function getOffers(): array
+    {
+        return $this->offers;
+    }
 
-	/**
-	 * @return mixed|null
-	 */
-	public function getShopOfTheYear()
-	{
-		return $this->shopOfTheYear;
-	}
+    /**
+     * @param Offer[] $offers
+     */
+    public function setOffers(array $offers): void
+    {
+        $this->offers = $offers;
+    }
 
+    public function addOffer(Offer $offer): void
+    {
+        $this->offers[] = $offer;
+    }
 
-	public function getRating(): ?int
-	{
-		return $this->rating;
-	}
-
-
-	public function getRatingCount(): ?int
-	{
-		return $this->ratingCount;
-	}
-
-
-	public function getReviewCount(): ?int
-	{
-		return $this->reviewCount;
-	}
-
-
-	public function isCashBackQuarantee(): bool
-	{
-		return $this->cashBackQuarantee;
-	}
 }
